6ee2a75d96d7b527491755f0ef0c1b5dd5ee4073
Riverdave <davidriverg@gmail.com>
2024-07-21 03:07:54 -0400

    Initial commit

---
+++ app/docs/page.tsx
@@ -0,0 +1,15 @@
+
+const Docs = () => (
+
+
+  <main className="">
+    <div>
+      <h1 className='flex flex-col items-center justify-between'>Docs Page</h1>
+      <p className="flex justify-end">This is the docs page.</p>
+    </div>
+
+
+  </main>
+);
+
+export default Docs;
