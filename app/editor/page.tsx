0a90dc8370d4b4d84a8fdf080f38ad82c748ad1b
Riverdave <davidriverg@gmail.com>
2024-07-24 04:13:23 -0400

    Clear unused files & clear dead code

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -12,20 +12,21 @@ export default function Editor() {
   const [inputValue, setInputValue] = useState<string>('')
   const [outputValue, setOutputValue] = useState<string>('')
 
-  //Could assess the bad sharing state by enrolling these in a context provider 
+  //Could assess the bad state sharing and excess use of client rendering
+  //by enrolling the main components in a context provider
   return (
     <div className="flex-col justify-center">
 
       <div className="justify-center align-middle">
-        <InputArea key={0} content={inputValue} setUserInput={setInputValue} /> {/* Bad Practice, dont pass setter! */}
+        <InputArea  content={inputValue} setUserInput={setInputValue} /> {/* Bad Practice, dont pass setter! */}
       </div>
 
       <div className="flex justify-center">
-        <RunBtn key={1} content={inputValue} setOutputBox={setOutputValue} />
+        <RunBtn  content={inputValue} setOutputBox={setOutputValue} />
       </div>
 
       <div className="border border-black h-48">
-        <OutputBox key={2} content={outputValue} setOutputBox={undefined} />
+        <OutputBox  content={outputValue} setOutputBox={undefined} />
       </div>
 
     </div>
